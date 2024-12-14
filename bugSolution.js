9f28f94d2c8b1f27ccce3a042d028250fec38bc5
BugHunter-X <askzenai@gmail.com>
2024-12-14 14:00:08 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,29 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="/about">Go to About</Link>
+    </div>
+  );
+}
+
+function About() {
+  return <div>About</div>;
+}
+```
+This corrected code uses nested routes properly by placing the `<Route path="/about" ...` within the parent route (`<Route path="/" ...>`). This resolves the routing issues present in the original code.
